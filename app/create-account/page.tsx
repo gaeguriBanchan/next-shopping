266232fa93f8368b7b491cbaf4f0524e862a3fff
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-06 16:48:39 +0900

    LoginScreen

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,7 +1,6 @@
 import FormBtn from '@/components/form-btn';
 import FormInput from '@/components/form-input';
-import { ChatBubbleOvalLeftEllipsisIcon } from '@heroicons/react/16/solid';
-import Link from 'next/link';
+import SocialLogin from '@/components/social-login';
 
 export default function CreateAccount() {
   return (
@@ -27,18 +26,7 @@ export default function CreateAccount() {
         />
         <FormBtn loading={true} text="Create Account" />
       </form>
-      <div className="w-full h-px bg-gray-500" />
-      <div>
-        <Link
-          className="primary-btn flex h-10 items-center justify-center gap-2"
-          href="/sms"
-        >
-          <span>
-            <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />
-          </span>
-          <span>Sign up with SMS</span>
-        </Link>
-      </div>
+      <SocialLogin />
     </div>
   );
 }
