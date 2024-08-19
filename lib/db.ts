0104ef054d99143b5c8c08f239ededbaf9852d6c
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-19 23:40:07 +0900

    Prisma Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -2,4 +2,30 @@ import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
+async function test() {
+  // <Create>
+  // const token = await db.sMSToken.create({
+  //   data: {
+  //     token: '121212',
+  //     user: {
+  //       connect: {
+  //         id: 1,
+  //       },
+  //     },
+  //   },
+  // });
+  // <Find>
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+
+test();
+
 export default db;
