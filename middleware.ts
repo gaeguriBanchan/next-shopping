19273531ede766fe7ab1268f7b6205087ab459bf
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-21 15:21:03 +0900

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
 import { NextRequest } from 'next/server';
-import getSession from './lib/session';
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === '/profile') {
-    return Response.redirect(new URL('/', request.url));
-  }
+  console.log('hello');
 }
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
