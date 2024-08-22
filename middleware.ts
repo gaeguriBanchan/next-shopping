f0fb2646d17f5f87fe43e71fceb99838b28c6c8b
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-22 12:02:27 +0900

    Github Authentication

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,8 @@ const publicOnlyUrls: IRoutes = {
   '/login': true,
   '/sms': true,
   '/create-account': true,
+  '/github/start': true,
+  '/github/complete': true,
 };
 
 export async function middleware(request: NextRequest) {
