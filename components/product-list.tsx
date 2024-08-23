bc3bb99fec44afabc8893fa4508181cbdf7f7472
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-23 16:49:57 +0900

    Pagination Actions 2

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -19,10 +19,17 @@ interface IProductListProps {
 export default function ProductList({ initialProducts }: IProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [isLastPage, setIsLastPage] = useState(false);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
-    const newProducts = await getMoreProducts(1);
-    setProducts((prev) => [...prev, ...newProducts]);
+    const newProducts = await getMoreProducts(page + 1);
+    if (newProducts.length !== 0) {
+      setPage((prev) => prev + 1);
+      setProducts((prev) => [...prev, ...newProducts]);
+    } else {
+      setIsLastPage(true);
+    }
     setIsLoading(false);
   };
   return (
@@ -30,13 +37,17 @@ export default function ProductList({ initialProducts }: IProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-      >
-        {isLoading ? '로딩 중' : 'Load more'}
-      </button>
+      {isLastPage ? (
+        <h2>마지막 상품입니다.</h2>
+      ) : (
+        <button
+          onClick={onLoadMoreClick}
+          disabled={isLoading}
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+        >
+          {isLoading ? '로딩 중' : 'Load more'}
+        </button>
+      )}
     </div>
   );
 }
