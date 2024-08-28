354c5b97353c9701cf0385ef3f862175059c04e9
gaegulgaegul <107011288+gaeguriBanchan@users.noreply.github.com>
2024-08-28 15:54:56 +0900

    revalidatePath

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -3,19 +3,13 @@ import ProductList from '@/components/product-list';
 import db from '@/lib/db';
 import { PlusIcon } from '@heroicons/react/24/solid';
 import { Prisma } from '@prisma/client';
-import { unstable_cache } from 'next/cache';
+import { revalidatePath, unstable_cache } from 'next/cache';
 import Link from 'next/link';
 
-const getCachedProducts = unstable_cache(
-  getInitialProducts,
-  ['home-products'],
-  // revalidate: 다시 요청이 왔을때 이전 요청이 60초가 넘었다면 다시 호출.
-  {
-    revalidate: 60,
-  }
-);
+const getCachedProducts = unstable_cache(getInitialProducts, ['home-products']);
 
 async function getInitialProducts() {
+  console.log('hit!!');
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -42,9 +36,17 @@ export const metadata = {
 
 export default async function Products() {
   const initialProducts = await getCachedProducts();
+  const revalidate = async () => {
+    'use server';
+    // revalidatePath : 경로과 관련된 모든 cache를 revalidate.
+    revalidatePath('/home');
+  };
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <form action={revalidate}>
+        <button>revalidate</button>
+      </form>
       <div className="flex max-w-screen-sm justify-end p-5">
         <Link
           href="/products/add"
